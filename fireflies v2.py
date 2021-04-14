ab918da1aad59e75f63c64d7d4566a04193ebc8b
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-04-14 21:30:59 +0200

    init

--- fireflies v2.py
+++ fireflies v2.py
@@ -1,4 +1,9 @@
-# Write your code here :-)
+# Name: Fireflies
+# Coder: Marco Janssen (twitter @marc0janssen)
+# date: 2021-03-30
+# version: 1.0.0
+
+
 from microbit import display, sleep
 import random
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+Name: Fireflies
+Coder: Marco Janssen (twitter @marc0janssen)
+date: 2021-03-30
+version: 1.0.0
