1efa8a5813e9019a1bdb697135fd038f2cdc83c0
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-04-14 23:36:18 +0200

    fixes

--- fireflies v2.py
+++ fireflies v2.py
@@ -1,34 +1,11 @@
 # Name: Fireflies
 # Coder: Marco Janssen (twitter @marc0janssen)
 # date: 2021-03-30
-# version: 1.0.2
+# version: 1.0.5
 
 
 from microbit import display, sleep
 from random import randint
-
-# This fucntions fixes the "display.get_pixel" bug.
-# a full bright led does not return a "9" but a "255".
-
-
-def fixPixelBug(brightness):
-    if brightness == 4:
-        return 3
-    elif brightness == 8:
-        return 4
-    elif brightness == 16:
-        return 5
-    elif brightness == 32:
-        return 6
-    elif brightness == 64:
-        return 7
-    elif brightness == 128:
-        return 8
-    elif brightness == 255:
-        return 9
-    else:
-        return brightness
-
 
 while True:
     sleep(50)
@@ -43,5 +20,5 @@ while True:
         x = randint(0, 4)
         y = randint(0, 4)
 
-        if fixPixelBug(display.get_pixel(x, y)) > 0:
-            display.set_pixel(x, y, fixPixelBug(display.get_pixel(x, y)) - 1)
+        if display.get_pixel(x, y) > 0:
+            display.set_pixel(x, y, display.get_pixel(x, y) - 1)
