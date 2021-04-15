34577ec8793291b22e49ac2173721e895bc849e9
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-04-15 07:27:45 +0200

    simplified

--- fireflies v2.py
+++ fireflies v2.py
@@ -1,7 +1,7 @@
 # Name: Fireflies
 # Coder: Marco Janssen (twitter @marc0janssen)
 # date: 2021-03-30
-# version: 1.0.5
+# version: 1.0.6
 
 
 from microbit import display, sleep
@@ -20,5 +20,5 @@ while True:
         x = randint(0, 4)
         y = randint(0, 4)
 
-        if display.get_pixel(x, y) > 0:
-            display.set_pixel(x, y, display.get_pixel(x, y) - 1)
+        brightness = max(display.get_pixel(x, y), 1)
+        display.set_pixel(x, y, brightness - 1)
